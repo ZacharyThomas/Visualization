417bb93bf771c2dc41090e83bdc6090d3554757a
Zack Smith <zack.smith@temple.edu>
2014-10-20 20:31:27 -0400

    Fixed analytics things.

--- bing_analytics/lol_bing.py
+++ bing_analytics/lol_bing.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-#Gets commute data from the zip codes of a csv over the span of a week
-
 import csv
 import urllib
 import json
@@ -11,7 +9,7 @@ from time import strftime
 
 workers=[]
 unique_zips = []
-api_key = "Ar5IGdiMiQdXWyvdFbt97lkcBfu0jiNQ91TyluFQzYFRN3WhR_VsH56_pjLxUBKg "
+api_key = "Ar5IGdiMiQdXWyvdFbt97lkcBfu0jiNQ91TyluFQzYFRN3WhR_VsH56_pjLxUBKg"
 
 #read in csv file rows into array
 with open('merck.csv') as f:
@@ -68,22 +66,6 @@ def get_commute(start_loc, end_loc):
 	commute.append(data["resourceSets"][0]["resources"][0]["travelDistance"])
 
 	return commute
-	
-# get commute distance and time in seconds
-# returns a list [distance, time]	
-def get_points(start_loc, end_loc):
-	URL = "http://dev.virtualearth.net/REST/V1/Routes/Driving?&wp.0="+start_loc+"&wp.1="+end_loc+"&routePathOutput=Points&key="+api_key
-	
-	bingResponse = urllib.urlopen(URL)
-	data = json.loads(bingResponse.read())
-	
-	points = []
-	
-	#get lat/long points
-	
-	points.append(data["resourceSets"][0]["resources"][0]["routePath"]["line"]["coordinates"])
-	
-	return points
     
 #######################################
 # MAIN
@@ -92,19 +74,44 @@ def get_points(start_loc, end_loc):
 kenilworth_NJ = '07033'
 error_zips = []
 
-#Print points for each route to file	
-for zips in unique_zips:
-	print zips
-	try:
-		points = get_points(zips, kenilworth_NJ)
-		with open('route_points.txt', 'a') as myFile:
-			myFile.write("Zip:"+zips + "  " + ''.join(str(points)) + "\n")
+
+with open('commute_times.txt', 'a') as myFile:
+	myFile.write("Zipcode  Duration (s)  Distance\n")
+myFile.close()
+
+start_time = strftime("%m-%d-%Y %H:%M")
+
+#1 week from start
+end_time = start_time.replace(strftime("-%d-"), str(int(strftime("%d"))+7))
+
+while True:
+	#exits after a week
+	if strftime("%m-%d-%Y %H:%M") is end_time:
+		exit()
+		
+	#Gets data every half hour and hour
+	if "00" in strftime("%M") or "30" in strftime("%M"):
+		loopcount += 1
+		
+		#print time to file
+		with open('commute_times.txt', 'a') as myFile:
+			myFile.write("Time: " +strftime("%m-%d-%Y %H:%M:%S") + "\n")
 		myFile.close()
-		time.sleep(0.1) #API has a 10 requests per second limit
-	except IndexError:
-		#error_zips.append(zips)
-		print "IndexError!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	except KeyError:
-		print "KeyError!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+		
+		#Print data for each zip to file	
+		for zips in unique_zips:
+			print zips
+	
+			try:
+				commute = get_commute(zips, kenilworth_NJ)
+				with open('commute_times.txt', 'a') as myFile:
+					myFile.write(zips + "  " + str(commute[0]) + "  " + str(commute[1]) + "\n")
+				myFile.close()
+				time.sleep(0.1) #API has a 10 requests per second limit
+			except IndexError:
+				#error_zips.append(zips)
+				donothing=1
+
+
 
 
