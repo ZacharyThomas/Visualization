a85760bb0a56b975619bc8acd6dc05e0272cf79d
jeprei <jeprei@hotmail.com>
2014-10-29 19:11:07 -0400

    Create general outline for poly line overlay with location marker

--- gmaps2/analytics/poly.py
+++ gmaps2/analytics/poly.py
@@ -9,16 +9,19 @@ Created on Oct 29, 2014
 import sys
 
 def main():
-    # open first file containing original routes
+    # open first file containing original route points
     with open(sys.argv[1], 'r') as f1:
+        # second file will contain paths
         with open(sys.argv[2], 'w') as f2:
+            # third file will contain js to draw polylines
             with open(sys.argv[3], 'w') as f3:
                 count=0
                 for lines in f1:
                     count = count + 1
                     f2.write('path%d = '%count + lines[12:-2] + ';\n')
                     
-                    f3.write("    map.drawPolyline({\n        path: path%d,\n        strokeColor: '#131540',\n        strokeOpacity: 0.6,\n        strokeWeight: 6\n      });\n" %count)
+                    f3.write("    map.drawPolyline({\n        path: path%d,\n        strokeColor: '#131540',\n        strokeOpacity: 0.3,\n        strokeWeight: 3\n      });\n" %count)
 
 if __name__=="__main__":
     main()
+
