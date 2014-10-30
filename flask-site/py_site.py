61f2e82ed0ca7bf34a34f62f448861860eb32944
Zack_Smith <zack.smith@temple.edu>
2014-10-30 13:21:49 -0400

    Added both html files to website, /white and / index these files.

--- flask-site/py_site.py
+++ flask-site/py_site.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Return HTML page 
-    return render_template('poly_loc1.html')
+    return render_template('poly_kenil.html')
 
+@app.route('/white')
+def white():
+    return render_template('poly_white.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
